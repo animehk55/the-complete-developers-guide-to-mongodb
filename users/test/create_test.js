032fa78ebb0bed31f1acd74a817389c988ff8df8
ANIMESH KUMAR <animeshk@webelight.com>
2019-01-19 21:05:54 +0530

    Added test successfully - Mocha

---
+++ users/test/create_test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating records', () => {
+    it('Saves a user', () => {
+        assert( 1 + 1 === 3);
+    });
+});
